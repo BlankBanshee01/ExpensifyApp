7a1076c0772edd4ce01db1cc6ba5db838f74ef84
BlankBanshee01 <ettibi.96@outlook.com>
2020-08-13 16:19:50 +0100

    tried fix of env var in heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,16 @@
 import * as firebase from "firebase";
 
+const env = runtimeEnv();
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: env.REACT_APP_APIKEY,
+  authDomain: env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: env.REACT_APP_DATABASE_URL,
+  projectId: env.REACT_APP_PROJECT_ID,
+  storageBucket: env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: env.REACT_APP_ID,
+  measurementId: env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
